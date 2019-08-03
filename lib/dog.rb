7710c5ce68ca7d9b60f171be68d8876b9392cc62
Adam Schonely <aschonely@gmail.com>
2019-08-03 02:35:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   end  
   
   def name
-    this_dogs_name
+    @this_dogs_name
   end
   
   def breed=(dogs_breed) 
@@ -13,7 +13,7 @@ class Dog
   end
   
   def breed
-    this_dogs_breed
+    @this_dogs_breed
   end  
   
 end  
